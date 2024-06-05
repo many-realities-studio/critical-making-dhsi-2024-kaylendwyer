20cc67fb47d1087eeb6e4ee0a40bd685e6a2575f
kaylendwyer <kaylendwyer@gmail.com>
2024-06-05 15:16:35 -0700

    iterate on collissions

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 This repository contains sketches, demos and exercises for Digital Media 5508: Programming for Digital Media.
 
 You should add your sketchbook documentation to the sketchbook directory, and your labs in the labs subdirectory. 
+
+Kaylen Dwyer
--- sketchbook/1/script.js
+++ sketchbook/1/script.js
@@ -1,9 +1,177 @@
+let squares = [];
+let debris = [];
+
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  print("Hello world")
+  createCanvas(600, 400);
 }
 
 function draw() {
-  // Draw a circle in the center of the screen
-  circle(width / 2, height / 2, 50)
+  background(220);
+
+  // Add new squares at intervals
+  if (frameCount % 60 == 0) { // Slower rate
+    squares.push(new Square(random(width - 40), 0, 40));
+  }
+
+  // Update and display squares
+  for (let i = squares.length - 1; i >= 0; i--) {
+    let square = squares[i];
+    square.update();
+    square.show();
+    if (square.isShattered && square.pieces.length === 0) {
+      squares.splice(i, 1); // Remove square
+    }
+  }
+
+  // Update and display debris
+  for (let i = debris.length - 1; i >= 0; i--) {
+    let piece = debris[i];
+    piece.update();
+    piece.show();
+  }
+
+  // Handle collisions between debris pieces
+  handleCollisions();
+}
+
+class Square {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.speed = 2; // Reduced speed for slower fall
+    this.isShattered = false;
+    this.pieces = [];
+  }
+
+  update() {
+    if (!this.isShattered) {
+      this.y += this.speed;
+      if (this.y >= height - this.size) {
+        this.y = height - this.size;
+        this.shatter();
+      }
+    } else {
+      for (let piece of this.pieces) {
+        piece.update();
+      }
+    }
+  }
+
+  show() {
+    if (!this.isShattered) {
+      fill(0);
+      rect(this.x, this.y, this.size, this.size);
+    } else {
+      for (let piece of this.pieces) {
+        piece.show();
+      }
+    }
+  }
+
+  shatter() {
+    this.isShattered = true;
+    let pieceSize = this.size / 4; // Smaller pieces
+    for (let i = 0; i < 16; i++) { // Create 16 pieces
+      let offsetX = (i % 4) * pieceSize;
+      let offsetY = floor(i / 4) * pieceSize;
+      let piece = new Piece(this.x + offsetX, this.y + offsetY, pieceSize);
+      this.pieces.push(piece);
+      debris.push(piece); // Add piece to debris
+    }
+  }
+}
+
+class Piece {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.speedX = random(-2, 2);
+    this.speedY = random(-5, -1); // Initial upward speed for bounce
+    this.gravity = 0.2; // Gravity effect
+  }
+
+  update() {
+    this.speedY += this.gravity;
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    // Constrain to canvas boundaries
+    if (this.x < 0) {
+      this.x = 0;
+      this.speedX *= -0.5; // Reverse and reduce speed
+    } else if (this.x + this.size > width) {
+      this.x = width - this.size;
+      this.speedX *= -0.5; // Reverse and reduce speed
+    }
+
+    if (this.y + this.size > height) {
+      this.y = height - this.size;
+      this.speedY *= -0.5; // Reduce speed and reverse direction
+      this.speedX *= 0.8; // Reduce horizontal speed
+      if (abs(this.speedY) < 1) {
+        this.speedY = 0; // Stop bouncing when speed is very low
+        this.speedX = 0;
+      }
+    }
+  }
+
+  show() {
+    fill(100);
+    rect(this.x, this.y, this.size, this.size);
+  }
+}
+
+function handleCollisions() {
+  for (let i = 0; i < debris.length; i++) {
+    for (let j = i + 1; j < debris.length; j++) {
+      let pieceA = debris[i];
+      let pieceB = debris[j];
+
+      if (isColliding(pieceA, pieceB)) {
+        resolveCollision(pieceA, pieceB);
+      }
+    }
+  }
+}
+
+function isColliding(pieceA, pieceB) {
+  return !(
+    pieceA.x + pieceA.size < pieceB.x ||
+    pieceA.x > pieceB.x + pieceB.size ||
+    pieceA.y + pieceA.size < pieceB.y ||
+    pieceA.y > pieceB.y + pieceB.size
+  );
+}
+
+function resolveCollision(pieceA, pieceB) {
+  let overlapX = min(pieceA.x + pieceA.size - pieceB.x, pieceB.x + pieceB.size - pieceA.x);
+  let overlapY = min(pieceA.y + pieceA.size - pieceB.y, pieceB.y + pieceB.size - pieceA.y);
+
+  let maxOverlap = pieceA.size / 2; // 50% overlap
+
+  if (overlapX < overlapY) {
+    if (overlapX > maxOverlap) overlapX = maxOverlap;
+    if (pieceA.x < pieceB.x) {
+      pieceA.x -= overlapX / 2;
+      pieceB.x += overlapX / 2;
+    } else {
+      pieceA.x += overlapX / 2;
+      pieceB.x -= overlapX / 2;
+    }
+  } else {
+    if (overlapY > maxOverlap) overlapY = maxOverlap;
+    if (pieceA.y < pieceB.y) {
+      pieceA.y -= overlapY / 2;
+      pieceB.y += overlapY / 2;
+    } else {
+      pieceA.y += overlapY / 2;
+      pieceB.y -= overlapY / 2;
+    }
+
+    // Adjust vertical speed to simulate bounce effect
+    pieceA.speedY *= -0.5;
+    pieceB.speedY *= -0.5;
+  }
 }
